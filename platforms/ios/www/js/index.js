68fc3c11f3f0725070764108e50733743aad1dee
Juan M. Chavez Jr <juan@dexteroustechnology.com>
2015-03-13 14:20:08 -0700

    move all to iOS platform

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        initPushwoosh();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,5 +48,38 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+function initPushwoosh() {
+    var pushNotification = window.plugins.pushNotification;
+
+    //set push notification callback before we initialize the plugin
+    document.addEventListener('push-notification', function (event) {
+        //get the notification payload
+        var notification = event.notification;
+
+        //display alert to the user for example
+        alert(notification.aps.alert);
+
+        //clear the app badge
+        pushNotification.setApplicationIconBadgeNumber(0);
+    });
+
+    //initialize the plugin
+    pushNotification.onDeviceReady({ pw_appid: "AA6F3-F5940" });
+
+    //register for pushes
+    pushNotification.registerDevice(
+        function (status) {
+            var deviceToken = status['deviceToken'];
+            console.warn('registerDevice: ' + deviceToken);
+        },
+        function (status) {
+            console.warn('failed to register : ' + JSON.stringify(status));
+            alert(JSON.stringify(['failed to register ', status]));
+        }
+    );
+
+    //reset badges on app start
+    pushNotification.setApplicationIconBadgeNumber(0);
+}
 
 app.initialize();
