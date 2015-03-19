dab5e453882862750c3ab5edae5b9c97e81129da
Juan <juan@dexteroustechnology.com>
2015-03-19 13:49:44 -0700

    change pushwosh ID

--- www/js/PushwooshiOS.js
+++ www/js/PushwooshiOS.js
@@ -39,7 +39,7 @@ function registerPushwooshIOS() {
 	);
 
 	//initialize the plugin
-    pushNotification.onDeviceReady({pw_appid:"539E9-AB8AE"});
+	pushNotification.onDeviceReady({pw_appid: "AA6F3-F5940"});
 
 	//register for pushes
 	pushNotification.registerDevice(
